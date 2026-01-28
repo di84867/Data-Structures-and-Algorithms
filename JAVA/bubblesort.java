acd5151d8bd69a5890e982494af2fead0b523a92
Divyansh Singh <singhdivyansh84867@gmail.com>
2026-01-28 11:39:39 +0530

    feat: add Java implementation of the bubble sort algorithm with user input and output

--- JAVA/bubblesort.java
+++ JAVA/bubblesort.java
@@ -10,11 +10,11 @@ class bubblesort {
             int[] arr = new int[n];
    
             System.out.println("Enter the elements till size " + n);
-            for(i = 0; i < n; i++) {
+            for(int i = 0; i < n; i++) {
                 arr[i] = sc.nextInt();
             }
-            for(i = 0; i < n - 1; i++) {
-                for(j = 0; j < n - i - 1; j++) {
+            for(int i = 0; i < n - 1; i++) {
+                for(int j = 0; j < n - i - 1; j++) {
                     if(arr[j] > arr[j + 1]) {
                         temp = arr[j];
                         arr[j] = arr[j + 1];
@@ -23,7 +23,7 @@ class bubblesort {
                 }
             }
             System.out.print("Final array after all operations: ");
-            for(i = 0; i < n; i++) {
+            for(int i = 0; i < n; i++) {
                 System.out.print(arr[i] + " ");
             }
         }
