2ee7c696366c5fbf3b5b01419089e5d7ea4251b1
Divyansh Singh <singhdivyansh84867@gmail.com>
2026-03-28 16:58:28 +0530

    refactor: Consolidate matrix operations including printing, sums, diagonals, and transpose into a single `matrix.java` file.

--- MinCost.java
+++ MinCost.java
@@ -1,41 +1,27 @@
 import java.util.Scanner;
 
-public class MinCost {
-
-    public static void sortDescending(int[] a) {
-        int n = a.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (a[j] < a[j + 1]) { 
-                    int temp = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = temp;
-                }
-            }
-        }
-    }
-
-    public static int calculateCost(int[] a) {
-        int cost = 0;
-        for (int i = 0; i < a.length; i++) {
-            cost += (i + 1) * a[i];
-        }
-        return cost;
-    }
-    public static void main(String[] args) {
+class MinCost {
+    public static void main(String[] xyz) {
         Scanner sc = new Scanner(System.in);
- 
-        System.out.print("Enter number of elements: ");
+        System.out.print("Enter the number of elements in array: ");
         int n = sc.nextInt();
-        int[] a = new int[n];
-
-        System.out.println("Enter the elements:");
+        int a[] = new int[n];
+        System.out.println("Enter the elements in an array");
         for (int i = 0; i < n; i++) {
             a[i] = sc.nextInt();
         }
-        sortDescending(a);
-        int totalCost = calculateCost(a);
-        System.out.println("Total cost = " + totalCost);
+        for (int i = 1; i < n; i++) {
+            if (a[i] < a[i + 1]) {
+                int temp = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = temp;
+            }
+        }
+        int cost = 0;
+        for (int i = 1; i <= n; i++) {
+            cost += (i + 1) * a[i];
+        }
+        System.out.println("The cost of deleting the array is: " + cost);
         sc.close();
     }
 }
