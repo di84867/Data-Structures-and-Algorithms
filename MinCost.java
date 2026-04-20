3200744119599ca1f2dc7cbff3733669d996bf99
Divyansh Singh <singhdivyansh84867@gmail.com>
2026-04-20 17:15:59 +0530

    refactor: update MinCost and nobleint to use ArrayLists and improve code formatting

--- MinCost.java
+++ MinCost.java
@@ -2,24 +2,30 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-class MinCost{
-    public static void main(String[] xyz){
-        Scanner sc=new Scanner(System.in);
+
+class MinCost {
+    public static void main(String[] xyz) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter elements in an array");
-        ArrayList<Integer> a=new ArrayList<>();
-        while(sc.hasNextInt()){
+        ArrayList<Integer> a = new ArrayList<>(); // this is ArrayList which is used to declare dynamic sized array
+        while (sc.hasNextInt()) {
             a.add(sc.nextInt());
         }
         System.out.println("The entered array:-");
-        for(int i=0;i<a.size();i++){
-            System.out.print(a.get(i)+" ");
+        for (int i = 0; i < a.size(); i++) {
+            System.out.print(a.get(i) + " ");
         }
         System.out.println();
         Collections.sort(a, Collections.reverseOrder());
-        int cost=0;
-        for(int i=0;i<a.size();i++){
-            cost+=(i+1)*a.get(i);   
+        System.out.println("The sorted array:-");
+        for (int i = 0; i < a.size(); i++) {
+            System.out.print(a.get(i) + " ");
         }
-        System.out.println("The minimum cost is:- "+cost);
+        System.out.println();
+        int cost = 0;
+        for (int i = 0; i < a.size(); i++) {
+            cost += (i + 1) * a.get(i);
+        }
+        System.out.println("The minimum cost is:- " + cost);
     }
 }
--- nobleint.java
+++ nobleint.java
@@ -1,38 +1,29 @@
-import java.util.Scanner;
-public class nobleint {
-    public static void main(String[] args) {
-        int n;
-        boolean found = false;
+import java.util.*;
 
+class nobleint {
+    public static void main(String[] xyz) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter number of elements: ");
-        n = sc.nextInt();
-        int[] arr = new int[n];
-        System.out.println("Enter the elements:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        System.out.println("Enter elements in an array");
+        ArrayList<Integer> a = new ArrayList<>();
+        while (sc.hasNextInt()) {
+            a.add(sc.nextInt());
         }
-
-        sc.close();
-
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (arr[j] > arr[i]) {
-                    count++;
-                }
-            }
-
-            if (count == arr[i]) {
-                found = true;
-                System.out.println("Noble integer found: " + arr[i]);
-                break;
+        System.out.println("The entered array:-");
+        for (int i = 0; i < a.size(); i++) {
+            System.out.println(a.get(i) + " ");
+        }
+        Collections.sort(a);
+        System.out.println("The sorted array:-");
+        for (int i = 0; i < a.size(); i++) {
+            System.out.println(a.get(i) + " ");
+        }
+        int ans = 0;
+        for (int i = 0; i < a.size(); i++) {
+            if (i == a.get(i)) {
+                ans += 1;
             }
         }
-
-        if (!found) {
-            System.out.println("No noble integer found.");
-        }
+        System.out.println("The number of noble integers is:- ");
+        System.out.println(ans);
     }
 }
