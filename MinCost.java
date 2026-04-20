6bed70478dbca5f023e627b0f820a620f5d3d0d3
Divyansh Singh <singhdivyansh84867@gmail.com>
2026-04-20 11:57:04 +0530

    refactor: replace manual swap logic with nested loops for correct array sorting and update cost calculation index

--- MinCost.java
+++ MinCost.java
@@ -1,26 +1,33 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 class MinCost {
     public static void main(String[] xyz) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter the number of elements in array: ");
         int n = sc.nextInt();
         int a[] = new int[n];
+
         System.out.println("Enter the elements in an array");
         for (int i = 0; i < n; i++) {
             a[i] = sc.nextInt();
         }
-        for (int i = 1; i < n; i++) {
-            if (a[i] < a[i + 1]) {
-                int temp = a[i];
-                a[i] = a[i + 1];
-                a[i + 1] = temp;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (a[i] < a[j]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
             }
         }
         int cost = 0;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             cost += (i + 1) * a[i];
         }
+
         System.out.println("The cost of deleting the array is: " + cost);
         sc.close();
     }
