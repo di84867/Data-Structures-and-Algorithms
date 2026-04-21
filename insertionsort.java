3277a0f9cdbd9ddd16598b5eba77e3cda7f67f07
Divyansh Singh <singhdivyansh84867@gmail.com>
2026-04-21 13:59:00 +0530

    refactor: remove implementation from insertionsort and migrate selectionsort to use ArrayList

--- insertionsort.java
+++ insertionsort.java
@@ -1,30 +1,3 @@
-import java.util.Scanner;
-class insertionsort{
-    public static void main(String[] xyz){
-        try (Scanner sc = new Scanner(System.in)) {
-        int temp,n;
-        System.out.print("Enter the number of elements: ");
-        n = sc.nextInt();
-        int[] arr = new int[n];
-        System.out.println("Enter the elements: ");
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        for(int i=1;i<n;i++){
-            temp=arr[i];
-            int j=i-1;
-            while(j>=0 && arr[j]>temp){
-                arr[j+1]=arr[j];
-                j--;
-            }
-            arr[j+1]=temp;
-        }
-        System.out.println("Sorted array: ");
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i]+" ");
-        }
-        sc.close();
-        System.out.println();
-        }
-    }
+public class insertionsort {
+    
 }
--- selectionsort.java
+++ selectionsort.java
@@ -1,33 +1,39 @@
-import java.util.*;
-public class selectionsort {
-    public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Enter the number of elements: ");
-            int n = sc.nextInt();
-            int[] arr = new int[n];
-            for(int i = 0; i < n; i++) {
-                arr[i] = sc.nextInt();
-            }
-            selectionSort(arr);
-            System.out.println("Sorted array: ");
-            for(int i = 0; i < n; i++) {
-                System.out.print(arr[i] + " ");
-            }
+import java.util.Scanner;
+import java.util.ArrayList;
+class selectionsort{
+    public static void main(String[] xyz){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of an array");
+        int n=sc.nextInt();
+        int min_val;
+        int min_idx;
+        ArrayList<Integer> a=new ArrayList<>();
+        System.out.println("Enter the elements in an array");
+        for(int i=0;i<n;i++){
+            a.add(sc.nextInt()); //inserts element into a datastructure like arraylist etc.
         }
-    }
-    public static void selectionSort(int[] arr) {
-        int n = arr.length;
-        for(int i = 0; i < n-1; i++) {
-            int min_val=arr[i];
-            int min_idx = i;
-            for(int j = i+1; j < n; j++) {
-                if(arr[j] < min_val) {
-                    min_idx = j;
+        System.out.println("The entered array is:-");
+        for(int i=0;i<a.size();i++){
+            System.out.println(a.get(i)+" "); //get and set are used to access or update values of private variables.
+        }
+        for(int i=0;i<a.size()-1;i++){
+            min_val=a.get(i);
+            min_idx=i;
+            
+            for(int j=i+1;j<a.size();j++){
+                if(a.get(j)<min_val){
+                    min_val=a.get(j);
+                    min_idx=j;
                 }
             }
-            int temp = arr[min_idx];
-            arr[min_idx] = arr[i];
-            arr[i] = temp;
+            
+            int temp=a.get(i);
+            a.set(i,a.get(min_idx));
+            a.set(min_idx,temp);
+        }
+        System.out.println("Sorted array is:-");
+        for(int i=0;i<a.size();i++){
+            System.out.println(a.get(i)+" ");
         }
     }
 }
